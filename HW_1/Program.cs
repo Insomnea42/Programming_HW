935acf12defa284dd401976ee9e25ba3fbfd9290
insomnea42 <khveroe@gmail.com>
2022-08-12 14:41:14 +0500

    addint HW_1

---
+++ HW_1/Program.cs
@@ -0,0 +1,24 @@
+﻿int a = 0, b = 0;
+
+Console.Write("Введите число а: ");
+a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число b: ");
+b = Convert.ToInt32(Console.ReadLine());
+
+if (a == b)
+{
+    Console.WriteLine("Числа равны");
+}
+else{
+
+    if (a > b)
+    {
+        Console.WriteLine("max = " + a);
+    }
+    else
+    {
+        Console.WriteLine("max = " + b);
+
+    }
+}
