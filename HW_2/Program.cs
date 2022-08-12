854044074c973e5e4fc0d21042b283dc59a84e3e
insomnea42 <khveroe@gmail.com>
2022-08-12 14:55:55 +0500

    addint HW_2 and correctong HW_1

---
+++ HW_2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+int a = 0, b = 0, c = 0;
+int max = 0;
+
+Console.Write("Введите число а: ");
+a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число b: ");
+b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число c: ");
+c = Convert.ToInt32(Console.ReadLine());
+
+max = a;
+
+if (max < b)
+{
+    max = b;
+}
+if (max < c)
+{
+    max = c;
+}
+
+
+Console.WriteLine("max = " + max);
