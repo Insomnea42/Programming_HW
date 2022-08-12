2729ed88b0872d1bee118399ee6fdf23594260f1
insomnea42 <khveroe@gmail.com>
2022-08-12 15:14:52 +0500

    addint HW_3 and correctong HW_1

---
+++ HW_3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.Clear();
+
+bool chet = false;
+int a = 0, b = 0;
+
+Console.Write("Введите число а: ");
+a = Convert.ToInt32(Console.ReadLine());
+
+if (a == 0) { Console.WriteLine("Просьба ввести отличное от 0 значение"); }
+else
+{
+    b = a % 2;
+    if (b == 0) { chet = true; }
+
+
+    Console.WriteLine(chet);
+}
+
